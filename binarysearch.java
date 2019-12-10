fa5fd03f16ad8865bf3a8723cd09c27e8cfad771
mengjie wang <58456490+mjwang0905@users.noreply.github.com>
2019-12-10 11:59:25 -0800

    Create binarysearch.java

---
+++ binarysearch.java
@@ -0,0 +1,42 @@
+public class Binarysearch {
+	    // Returns index of x if it is present in array[l.. 
+	    // r], else return -1 
+	    int binarySearch(int arr[], int l, int r, int x) 
+	    { 
+	        if (r >= l) { 
+	            int mid = l + (r - l) / 2; 
+	  
+	            // If the element is present at the 
+	            // middle itself 
+	            if (arr[mid] == x) 
+	                return mid; 
+	  
+	            // If element is smaller than mid, then 
+	            // it can only be present in left spray 
+	            if (arr[mid] > x) 
+	                return binarySearch(arr, l, mid - 1, x); 
+	  
+	            // Else the element can only be present 
+	            // in right spray 
+	            return binarySearch(arr, mid + 1, r, x); 
+	        } 
+	  
+	        // We reach here when element is not present 
+	        // in array 
+	        return -1; 
+	    } 
+	  
+	    // Driver method to test above 
+	    public static void main(String args[]) 
+	    { 
+	        Binarysearch ob = new Binarysearch(); 
+	        int arr[] = { 1,5,9,4,3,8,7,6}; 
+	        int n = arr.length; 
+	        int x = 3; 
+	        int result = ob.binarySearch(arr, 0, n -1, x); 
+	        if (result == 1) 
+	            System.out.println("Element not present"); 
+	        else
+	            System.out.println("Element found at index " + result); 
+	    } 
+	}
